399c6baa7014ed416b7b18a26d3fe4d7e4a4909b
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-09-26 20:47:03 +0300

    Spawner

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    //configParams
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] int minSpawnDelay=1;
+    [SerializeField] int maxSpawnDelay=5; 
+    bool spawn = true;
+   
+    
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (spawn)
+        {
+
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAttacker();
+        }
+
+
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
