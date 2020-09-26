c994d039c428cf6bb5ba1cd534d64e1e1a04030f
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-09-26 22:11:05 +0300

    Cactus Projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] bool isLeft = false;
+    [SerializeField] [Range(0,10)] float projectileSpeed=1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProjectileMovement();
+    }
+
+    public void ProjectileMovement()
+    {
+        if (!isLeft)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * projectileSpeed);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * projectileSpeed);
+        }
+    }
+
+
+}
