18fff67e50654fe8c2e26a146ad2c286232727e8
Abdullah Akbaş <39574496+aakbas@users.noreply.github.com>
2020-09-28 16:13:45 +0300

    is Enemy in lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,54 @@ public class Shooter : MonoBehaviour
 
     [SerializeField] GameObject projectile;
     [SerializeField] GameObject gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
 
+    private void Start()
+    {
+        SetLaneSpawner();
+        animator = GetComponent<Animator>();
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
